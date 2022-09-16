1d22fb2a4314f799bf7ecd3e70741dbdc5b48e21
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2022-09-16 14:34:33 +0200

    Use proper list for listPage Better handling of TanStack query result (loading/error condition)

--- src/pages/list/ListPage.tsx
+++ src/pages/list/ListPage.tsx
@@ -1,26 +1,33 @@
-import usePokeList from "../../service/poke/usePokeList";
+import { useQuery } from "@tanstack/react-query";
+import { fetcher } from "../../core/fetcher";
+import { PokemonResultDto } from "../../service/poke/types";
 import PokeListEntry from "./poke-list-entry/PokeListEntry";
 import SearchPanel from "./search-panel/SearchPanel";
 import useDebouncedSearch from "./useDebouncedSearch";
+import styles from "./ListPage.module.scss";
 
 function ListPage() {
   const { searchTerm, setSearchTerm, debouncedSearchTerm } =
     useDebouncedSearch("");
 
-  const { pokemons, isLoading, error } = usePokeList();
+  const { data, isLoading, isError } = useQuery(["pokemon", "list"], () =>
+    fetcher<PokemonResultDto>("https://pokeapi.co/api/v2/pokemon?limit=1000")
+  );
+
+  if (isLoading) return <div>LOADING</div>;
+  if (isError) return <div>ERROR while loading data</div>;
+
   return (
-    <>
+    <div className={styles.root}>
       <SearchPanel searchTerm={searchTerm} onSearchChanged={setSearchTerm} />
-      {!isLoading &&
-        !error &&
-        pokemons
-          ?.filter((p) => p.name.includes(debouncedSearchTerm))
+      <ul>
+        {data.results
+          .filter((p) => p.name.includes(debouncedSearchTerm))
           .map((pokemon) => (
             <PokeListEntry key={pokemon.name} name={pokemon.name} />
           ))}
-      {isLoading && <div>LOADING</div>}
-      {error && <div>ERROR while loading data</div>}
-    </>
+      </ul>
+    </div>
   );
 }
 
